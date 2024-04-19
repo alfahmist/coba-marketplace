4486163527b29fc58d1eb4c4faabf0f14a14e953
alfahmist <gst.alfahmi@gmail.com>
2024-04-20 06:17:32 +0700

    Fitur Jumlah Barang

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,6 +1,21 @@
+import { useState } from 'react';
 import { Panel } from './Panel';
 
 const CartItem = () => {
+	const [count, setCount] = useState(1);
+	const productCountButton = (param) => {
+		if (param === 'tambah') {
+			if (count < 20) {
+				setCount(count + 1);
+			}
+		} else {
+			if (count > 0) {
+				setCount(count - 1);
+			}
+		}
+
+		console.log(count);
+	};
 	return (
 		<>
 			<div className='flex flex-row gap-8'>
@@ -23,14 +38,32 @@ const CartItem = () => {
 					</div>
 					<div className='flex flex-row gap-2 self-end'>
 						<button>Delete</button>
-						<div className='flex flex-row gap-4 border-2 rounded-lg'>
-							<button className='px-2 text-2xl'>-</button>
+						<div className='flex flex-row gap-2 border-2 rounded-lg'>
+							<button
+								className='px-2 text-xl'
+								onClick={() => productCountButton('minus')}
+							>
+								-
+							</button>
 							<input
 								type='text'
-								className='text-lg w-6 text-center'
-								value={1}
+								className='text-md w-8 text-center'
+								onChange={(event) => {
+									setCount(Number(event.target.value));
+								}}
+								onBlur={() => {
+									if (count > 20) {
+										setCount(20);
+									}
+								}}
+								value={count}
 							/>
-							<button className='px-2 text-2xl'>+</button>
+							<button
+								className='px-2 text-xl'
+								onClick={() => productCountButton('tambah')}
+							>
+								+
+							</button>
 						</div>
 					</div>
 				</div>
